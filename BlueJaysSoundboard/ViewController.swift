1bf0617357b4a9e9e48faba2c2b536ab456c1d64
Kevin Wong <kevin.jwong85@gmail.com>
2016-06-30 00:07:27 -0400

    Include button to play random soundbite
    There are a total of 12 soundbites for version 1.0 of the app. This
    diff includes the random button that plays 1 of the 12 soundbites

--- BlueJaysSoundboard/ViewController.swift
+++ BlueJaysSoundboard/ViewController.swift
@@ -6,12 +6,36 @@
 //  Copyright © 2016 Kevin Wong. All rights reserved.
 //
 
+import AVFoundation
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // Constants
+    let MP3 = "mp3"
+    let AUDIO_FILE_NAMES = ["1swingAndADrive",
+                            "2getUpBall",
+                            "3goodbyeHomeRun",
+                            "4batFlip",
+                            "5encarnacionSong",
+                            "6kawasaki",
+                            "7lilLooper",
+                            "8forgetAboutThisOne",
+                            "9ballgame",
+                            "10bangers",
+                            "11OKBlueJays",
+                            "12touchEmAllJoe"]
+    
+    // Variables
+    var avPlayer: AVAudioPlayer!
+    
+    // MARK: Properties
+    @IBOutlet weak var randomButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // Set background colour of app
         self.view.backgroundColor = UIColor(
             red:0,
             green:35/255.0,
@@ -34,8 +58,40 @@ class ViewController: UIViewController {
         return false
     }
     
+    // Keep device in portrait mode
     override func supportedInterfaceOrientations() -> UIInterfaceOrientationMask {
         return UIInterfaceOrientationMask.Portrait
     }
+    
+    // MARK: Actions
+    @IBAction func playRandomSound(sender: UIButton) {
+        // Generate a random soundbite to play
+        let audioFileNumber = arc4random_uniform(UInt32(AUDIO_FILE_NAMES.count))
+        let audioFile = AUDIO_FILE_NAMES[Int(audioFileNumber)]
+        
+        if let audioPlayer = getAVAudioPlayer(audioFile) {
+            avPlayer = audioPlayer
+            avPlayer.prepareToPlay()
+            avPlayer.play()
+        }
+    }
+    
+    // Helper function to create an AVAudioPlayer
+    func getAVAudioPlayer(fileName: String) -> AVAudioPlayer? {
+        var audioPlayer: AVAudioPlayer?
+        let url = NSURL.fileURLWithPath(
+            NSBundle.mainBundle().pathForResource(
+                fileName,
+                ofType: MP3)!)
+        
+        do {
+            try audioPlayer = AVAudioPlayer(contentsOfURL: url)
+        } catch {
+            print ("Could not create audio player.")
+        }
+        
+        return audioPlayer
+    }
+    
 }
 
